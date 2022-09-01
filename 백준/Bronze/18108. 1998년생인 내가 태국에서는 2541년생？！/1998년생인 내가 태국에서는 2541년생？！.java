e85292cc1d34487788650e70f9d962034df88cdf
LIM GUN WOO <76253790+lgw2238@users.noreply.github.com>
2022-09-01 10:53:25 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+        // 스캐너 
+		Scanner in = new Scanner(System.in);
+		
+		int year = in.nextInt();
+		// 서기 - 불기 : 543년 
+		System.out.println(year - 543);
+ 
+	}
+}
